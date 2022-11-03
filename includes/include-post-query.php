b20f55292eea13eb0286c66245a813ab0a23ae0f
Danial Bleile <danbleile@gmail.com>
2022-11-03 11:42:44 -0700

    Bug Fix | Public Pending | Add is admin conditional

--- includes/include-post-query.php
+++ includes/include-post-query.php
@@ -12,7 +12,7 @@ class Post_Query {
 
 	public static function add_pending_to_query( $query ) {
 
-		if ( $query->is_main_query() ) {
+		if ( $query->is_main_query() && is_admin() ) {
 
 			$query->set( 'post_status', array( 'publish', 'pending' ) );
 
